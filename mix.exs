3d14813a878ee9c067e03e67705d45b5745bf01f
Brian Glusman <brian@stellaservice.com>
2017-11-13 14:36:27 -0500

    Relax poison

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Exometer.NewrelicReporter.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.9.0"},
-      {:poison, "~> 2.0"},
+      {:poison, "~> 2.0 or ~> 3.0" },
       {:excoveralls, "~> 0.6", only: :test}
     ]
   end
